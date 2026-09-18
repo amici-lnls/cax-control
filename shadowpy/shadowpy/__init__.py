25a07f14550afc0ae8bc530f7d6273c9b3804e6b
Gabriel A. Amici <gabriel.amici@lnls.br>
2026-09-18 14:45:15 -0300

    massive commit: organizing the repo for future segmentation

--- shadowpy/shadowpy/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Module containing the wrapper classes around Shadow's Python API. 
-
-These classes are used to create a more user-friendly interface for the 
-Shadow library, and to provide additional functionality that is not 
-available in the original API.
-"""
